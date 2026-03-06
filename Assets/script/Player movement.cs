9592a6215d909ee24c9a84f7c8155c8ee7abb83d
ANHCUP <nguyenthianhly16@gmail.com>
2026-03-07 00:05:59 +0700

    animation nè
    animation run side, up
    code

--- Assets/script/Player movement.cs
+++ Assets/script/Player movement.cs
@@ -1,21 +1,43 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
     public float Speed;
     public Rigidbody2D rb;
     private SpriteRenderer sr;
+    public Animator ani;
+    Vector2 movement;
+
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
     }
 
-        void Update()
+    void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical   = Input.GetAxisRaw("Vertical");
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
 
-        rb.linearVelocity = new Vector2(horizontal * Speed, vertical * Speed);
+            ani.SetFloat("Horizontal", movement.x);
+            ani.SetFloat("Vertical", movement.y);
+            ani.SetFloat("Speed", movement.sqrMagnitude);
+
+        // Lật nhân vật khi đi trái phải
+        if (movement.x > 0)
+        {
+            sr.flipX = false;
+        }
+        else if (movement.x < 0)
+        {
+            sr.flipX = true;
+        }
+
+        // Sắp xếp layer theo trục Y 
         sr.sortingOrder = (int)(-transform.position.y * 100);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * Speed;
     }
 }

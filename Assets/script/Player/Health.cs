0e89b8ea7505f46f9004d950f49bb4da3fe47427
NhuLem <hsc57ch7yv@privaterelay.appleid.com>
2026-03-11 19:08:52 +0700

    anim hurt & dead cho player

---
+++ Assets/script/Player/Health.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+  public float maxHealth = 100f;
+    public float currentHealth;
+    public Slider healthBar; 
+
+    private Animator anim;
+    private bool isDead = false;
+
+    public MonoBehaviour playerMovementScript; 
+    public MonoBehaviour playerAttackScript;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>(); 
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (isDead) return;
+
+        currentHealth -= damage;
+        UpdateHealthBar();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            if (anim != null) anim.SetTrigger("Hurt");
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthBar != null)
+        {
+            healthBar.value = currentHealth / maxHealth;
+        }
+    }
+
+    void Die()
+    {
+        isDead = true;
+        
+        // 1. Kích hoạt anim ngã xuống
+        if (anim != null) anim.SetBool("isDead", true);
+
+        // 2. KHÓA ĐIỀU KHIỂN: Tắt các script di chuyển và đánh
+        if (playerMovementScript != null) playerMovementScript.enabled = false;
+        if (playerAttackScript != null) playerAttackScript.enabled = false;
+
+        // 3. Tắt va chạm (để quái không cắn xác Player nữa)
+        GetComponent<Collider2D>().enabled = false;
+
+        // 4. Xử lý Game Over (Ví dụ: Chờ 2 giây rồi tải lại màn chơi)
+        Debug.Log("Mage đã hy sinh!");
+        Invoke("ReloadGame", 3f); // Gọi hàm ReloadGame sau 2 giây
+    }
+
+    void ReloadGame()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
